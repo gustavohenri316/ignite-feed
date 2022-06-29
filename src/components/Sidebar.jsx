129f18db5e67a8f24c310890b5b1cf35faaa6c7b
gustavohenri316 <gustavohenri316@gmail.com>
2022-06-29 20:46:21 -0300

    sidebar parte 2

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,20 @@
 import styles from './Sidebar.module.css'
-
+import { PencilLine } from 'phosphor-react'
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <img 
+      <img
         className={styles.cover}
-        src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-1.2.1&divixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+        src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-1.2.1&divixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/gustavohenri316.png" />
         <strong>Gustavo Oliveira</strong>
         <span>Web Developer</span>
       </div>
       <footer>
         <a href="#">
+          <PencilLine size={20}/>
           Editar seu perfil
         </a>
       </footer>
