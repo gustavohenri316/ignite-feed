3fe45e1566e1fa6b44f2b01fb33cea88348adba0
gustavohenri316 <gustavohenri316@gmail.com>
2022-06-29 10:15:20 -0300

    sec

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
+import styles from './App.module.css'
 
 export function App() {
   return (
-    <>
+    <div>
       <Header />
-      <Post/>
-    </>
+      <div className={styles.wrapper}>
+        <aside>
+          sidebar
+        </aside>
+        <main>
+        <Post>
+
+        </Post>
+        </main>
+      </div>
+    </div>
   )
 }
 
