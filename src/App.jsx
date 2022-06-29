afa76a4fc8edd4712fdbe58b9d85758d063b8749
gustavohenri316 <gustavohenri316@gmail.com>
2022-06-29 20:23:52 -0300

    sidebar parte 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
       <div className={styles.wrapper}>
-        <aside>
-          sidebar
-        </aside>
+        <Sidebar/>
         <main>
         <Post>
 
