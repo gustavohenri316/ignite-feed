cab32dadb553ab82b148ab1479c54987f14b0b3c
gustavohenri316 <gustavohenri316@gmail.com>
2022-06-30 09:46:38 -0300

    adicionando map

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,50 @@ import { Post } from "./components/Post";
 import { Sidebar } from "./components/Sidebar";
 import styles from './App.module.css'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/gustavohenri316.png',
+      name: 'Gustavo Henrique',
+      role: 'Web developer'
+    },
+    content: [
+      { type: 'paragraph', content: "Fala galeraa 👋" },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Maik Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: "Fala galeraa 👋" },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00')
+  },
+]
 export function App() {
   return (
     <div>
       <Header />
       <div className={styles.wrapper}>
-        <Sidebar/>
+        <Sidebar />
         <main>
-        <Post/>
-        <Post/>
+          {posts.map(post => {
+            return (
+              <Post
+
+              />
+            )
+          })}
         </main>
       </div>
     </div>
