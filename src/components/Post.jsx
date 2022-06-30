ccd7c1488a23f1797a0c845b89cba2f55a52c40c
gustavohenri316 <gustavohenri316@gmail.com>
2022-06-29 21:20:38 -0300

    estilizacao do componente post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,7 +3,7 @@ import styles from './Post.module.css'
 export function Post() {
   return (
     <article className={styles.post}>
-      <header>
+      <header className={styles.header}>
         <div className={styles.author}>
           <img className={styles.avatar} src="https://github.com/gustavohenri316.png" />
           <div className={styles.authorInfo}>
@@ -19,8 +19,11 @@ export function Post() {
         <p>
           <p> Fala galeraa 👋</p>
           <p> Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-          <p> 👉 <a href="#"> jane.design/doctorcare </a></p>
-          <p> <a href="#">#novoprojeto </a><a href="#">#nlw</a><a href="#"> #rocketseat </a></p>
+          <p> 👉 {' '}<a href="#"> jane.design/doctorcare</a> {' '}</p>
+          <p> 
+            <a href="#">#novoprojeto</a>{' '}
+            <a href="#">#nlw</a>{' '}
+            <a href="#"> #rocketseat</a></p>
         </p>
       </div>
     </article>
