0d8a9483b39779d027d93194da7403ba9cf9e63b
gustavohenri316 <gustavohenri316@gmail.com>
2022-06-29 21:52:01 -0300

    estrutura de comentarios

---
+++ src/components/Comment.jsx
@@ -0,0 +1,30 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img className={styles.avatar} src="https://github.com/gustavohenri316.png" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Gustavo Henrique</strong>
+              <time title='11 de maio ás 08:13' dataTime="2022-05-11 08:13:30">Cerca de 1h atrás</time>
+            </div>
+            <button title='Deletar comentário'>
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+} 
