cd59b7ecd540c184669a5bb19e17e27fb2466fea
gustavohenri316 <gustavohenri316@gmail.com>
2022-06-29 21:04:49 -0300

    montando o html do componente post

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Header } from "./components/Header";
-import { Post } from "./Post";
-import styles from './App.module.css'
+import { Post } from "./components/Post";
 import { Sidebar } from "./components/Sidebar";
+import styles from './App.module.css'
 
 export function App() {
   return (
@@ -10,9 +10,7 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar/>
         <main>
-        <Post>
-
-        </Post>
+        <Post/>
         </main>
       </div>
     </div>
